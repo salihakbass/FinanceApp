b721d477607991d364d79a0da5b8ae76718931f2
Salih Akbas <salihhakbass@gmail.com>
2024-07-16 22:12:24 +0300

    BlurView dependency added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.github.Dimezis:BlurView:version-2.0.3")
 }
