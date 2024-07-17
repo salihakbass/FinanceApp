bcde9fb09b200b1edb3240456cf3fb60111bebf0
Salih Akbas <salihhakbass@gmail.com>
2024-07-17 12:35:28 +0300

    Glide dependency added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("com.github.Dimezis:BlurView:version-2.0.3")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
