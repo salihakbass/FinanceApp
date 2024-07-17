d52d5eb2ecc25acfb02276e823ec96c4b88b4d00
Salih Akbas <salihhakbass@gmail.com>
2024-07-17 12:32:55 +0300

    MainViewModel.kt created

---
+++ app/src/main/java/com/salihakbas/financeapp/viewmodel/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.salihakbas.financeapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.salihakbas.financeapp.repository.MainRepository
+
+class MainViewModel(val repository: MainRepository) : ViewModel() {
+    constructor() : this(MainRepository())
+
+    fun loadData() = repository.items
+}
